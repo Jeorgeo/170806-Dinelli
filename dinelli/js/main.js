9ba357a420e92fd3006ad4d0dab94f5e87293770
Yury_Shakhouski <Jeorgeo@list.ru>
2017-08-15 20:37:10 +0300

    170815-1800
    базовая разметка main-content

--- dinelli/js/main.js
+++ dinelli/js/main.js
@@ -1,6 +1,3 @@
-
-console.log('test');
-
 var toggle_btn = document.getElementById('js-toggle');
 var menu_nav = document.getElementById('masthead');
 var toggle_arrow = document.querySelector('.header-menu-toggle');
@@ -8,7 +5,6 @@ var toggle_arrow = document.querySelector('.header-menu-toggle');
 toggle_btn.addEventListener('click', moveMenu);
 
 function moveMenu(){
-  console.log(toggle_btn);
   if(menu_nav.classList.contains('nav-show')) {
       toggle_arrow.classList.remove('btn-arrow-top');
       toggle_arrow.classList.remove('btn-arrow-bottom');
